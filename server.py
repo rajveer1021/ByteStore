05361d251fa1d1c8747443cf397f9f89d2731d20
rajveer1021 <rajveermehta1021@gmail.com>
2026-06-27 20:49:00 +0530

    fix: handle connection reset errors gracefully

--- server.py
+++ server.py
@@ -13,26 +13,38 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     logger.info(f"Server started on {HOST}:{PORT}")
 
     def handle_client(conn, addr):
-        with conn :
-            logger.info(f"connected by {addr}")
+            with conn :
+                logger.info(f"connected by {addr}")
 
-            # Load the database from a JSON file
-            database = get_database()
-            
-            while True:
-                raw_data = conn.recv(2048)
-                parser = command_parser(raw_data)
-                if parser[3] is not None:
-                    conn.sendall(str(parser[3]).encode("utf-8"))
-                    continue
-                response = db_operation(action=parser[0], command=parser[1], value = parser[2], database=database)
+                # Load the database from a JSON file
+                database = get_database()
+
+                try:
+                    while True:
+                        raw_data = conn.recv(2048)
+                        parser = command_parser(raw_data)
+                        if parser[3] is not None:
+                            conn.sendall(str(parser[3]).encode("utf-8"))
+                            continue
+                        response = db_operation(action=parser[0], command=parser[1], value = parser[2], database=database)
+
+                        # Save the database to a JSON file
+                        save_database(database)
+
+                        if not raw_data:
+                            break
+                        conn.sendall(str(response).encode("utf-8"))
+
+                except ConnectionResetError:
+                    logger.info(f"Connection reset by {addr}")
                 
-                # Save the database to a JSON file
-                save_database(database)
-
-                if not raw_data:
-                    break
-                conn.sendall(str(response).encode("utf-8"))
+                except Exception as e:
+                    logger.error(f"Error occurred with {addr}: {e}")
+            
+                finally:
+                    # Save the database to a JSON file when the client disconnects
+                    save_database(database)
+                    logger.info(f"Connection closed by {addr}")
 
     # Start the server and listen for incoming connections
     while True:
